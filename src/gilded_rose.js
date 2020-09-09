691557574a26c71c2ac5e2fdd765d7d3b46a9060
Toon Janssens <toon@codibri.be>
2020-09-09 23:06:18 +0200

    Refactor updateQuality method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,57 +6,125 @@ class Item {
   }
 }
 
+class Rule {
+  constructor(condition, action) {
+    this.condition = condition;
+    this.action = action;
+  }
+
+  isApplicable(item) {
+    return this.condition(item);
+  }
+
+  apply(item) {
+    return this.action(item);
+  }
+}
+
+class RuleManager {
+  constructor(rules, guard) {
+    this.rules = rules;
+    this.guards = guard;
+  }
+
+  apply(item) {
+    return this.applyGuards(this.applyRules(item));
+  }
+
+  applyRules(item) {
+    for(let i = 0; this.rules.length; i++) {
+      const rule = this.rules[i];
+      if (rule.isApplicable(item)) {
+        return rule.apply(item);
+      }
+    };
+
+    return item;
+  }
+
+  applyGuards(item) {
+    this.guards.forEach(guard => {
+      item = guard(item);
+    });
+
+    return item;
+  }
+}
+
+const agedBrieRule = new Rule(
+  (item) => item.name === 'Aged Brie', 
+  (item) => {
+    item.quality++; 
+
+    return item;
+  }
+);
+
+const sulfurasRule = new Rule(
+  (item) => item.name === 'Sulfuras, Hand of Ragnaros',
+  (item) => item
+);
+
+const backstageTicketRule = new Rule(
+  (item) => item.name === 'Backstage passes to a TAFKAL80ETC concert',
+  (item) => {
+    if (item.sellIn <= 0) {
+      item.quality = 0;
+    } else if (item.sellIn <= 5) {
+      item.quality = item.quality + 3;
+    } else if (item.sellIn <= 10) {
+      item.quality = item.quality + 2;
+    }
+
+    return item;
+  }
+);
+
+const sellPassedRule = new Rule((item) => item.sellIn <= 0, (item) => {
+  item.quality = item.quality - 2;
+
+  return item;
+});
+
+const baseRule = new Rule(() => true, (item) => {
+  item.sellIn--; 
+  item.quality--;
+
+  return item;
+});
+
+const qualityGuard = (item) => {
+  if (item.quality < 0) {
+    item.quality = 0;
+  }
+  if (item.quality > 50) {
+    item.quality = 50;
+  }
+  return item;
+}
+
+const rulesManager = new RuleManager(
+  [
+    agedBrieRule, 
+    sulfurasRule, 
+    backstageTicketRule, 
+    sellPassedRule, 
+    baseRule
+  ], [
+    qualityGuard
+  ]
+);
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
+    this.items = this.items.map(item => {
+      return rulesManager.apply(item);
+      
+    });
+   
     return this.items;
   }
 }
